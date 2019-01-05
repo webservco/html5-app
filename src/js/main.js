6d757c39995405f3aa3659192c3478f1554c198c
Radu Maier <ariademur@webserv.co>
2019-01-05 10:20:46 +0100

    update main script

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,19 @@
 
 $(document).ready(function() {
     /* history navigation */
-    $("nav").historyNavigation({
+    $(document).historyNavigation({
         linkClass: "app-nav", // class of the links which trigger the navigation
         mainElementId: "main", // id of content wrapper element
         contentElementId: "content", // id of content element
         navigationElementClass: "navbar", // class of the element containing the navigation links
         onLinkClick: function( element ) { // callback
-            //console.log("callback: onLinkClick"); //XXX
-            //console.log(element); //XXX undefined
+            //console.log("callback: onLinkClick");
         },
         onUrlLoaded: function() { // callback
             //console.log("callback: onUrlLoaded");
         },
         onPopState: function( event ) { // callback
             //console.log("callback: onPopstate");
-            //console.log(event); //XXX undefined
         },
     });
 });
